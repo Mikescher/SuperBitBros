763d9e1cc844c44e7c6c2fc981927c37d4a10a53
Mike Schwörer <mailport@mikescher.de>
2013-09-21 22:11:39 +0200

    Missed a few files in last commit -.-

---
+++ ResourceAccessor.cs
@@ -0,0 +1,145 @@
+﻿using SuperBitBros.Properties;
+using System;
+
+namespace SuperBitBros
+{
+    public class ResourceAccessor
+    {
+        public static byte[] GetMap(int world, int lvl)
+        {
+            switch (world)
+            {
+                case 0:
+                    switch (lvl)
+                    {
+                        case 0:
+                            return Resources.map_debug;
+                        case 1:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        case 2:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        case 3:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        case 4:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        default:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                    }
+                case 1:
+                    switch (lvl)
+                    {
+                        case 1:
+                            return Resources.map_01_01;
+                        case 2:
+                            return Resources.map_01_02;
+                        case 3:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        case 4:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        default:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                    }
+                case 2:
+                    switch (lvl)
+                    {
+                        case 1:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        case 2:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        case 3:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        case 4:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        default:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                    }
+                case 3:
+                    switch (lvl)
+                    {
+                        case 1:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        case 2:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        case 3:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        case 4:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        default:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                    }
+                case 4:
+                    switch (lvl)
+                    {
+                        case 1:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        case 2:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        case 3:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        case 4:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        default:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                    }
+                case 5:
+                    switch (lvl)
+                    {
+                        case 1:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        case 2:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        case 3:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        case 4:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        default:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                    }
+                case 6:
+                    switch (lvl)
+                    {
+                        case 1:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        case 2:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        case 3:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        case 4:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        default:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                    }
+                case 7:
+                    switch (lvl)
+                    {
+                        case 1:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        case 2:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        case 3:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        case 4:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        default:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                    }
+                case 8:
+                    switch (lvl)
+                    {
+                        case 1:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        case 2:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        case 3:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        case 4:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                        default:
+                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                    }
+                default:
+                    throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+            }
+        }
+    }
+}
---
+++ Triggers/LevelWrapZone.cs
@@ -0,0 +1,49 @@
+﻿using SuperBitBros.Entities;
+using SuperBitBros.Entities.DynamicEntities;
+using SuperBitBros.OpenGL.OGLMath;
+using System.Drawing;
+
+namespace SuperBitBros.Triggers
+{
+    public class LevelWrapZone : Trigger
+    {
+        private const int ANY = 0;
+
+        private const int MASK_WORLD = 0xF0;
+        private const int MASK_LEVEL = 0x0F;
+
+        private int target_world;
+        private int target_level;
+
+        private bool active = true;
+
+        public LevelWrapZone(Vec2i pos, int ident)
+            : base(pos)
+        {
+            target_world = (ident & MASK_WORLD) >> 4;
+            target_level = (ident & MASK_LEVEL);
+        }
+
+        public override void OnCollide(DynamicEntity collider)
+        {
+            Player p = collider as Player;
+            if (p != null && active)
+            {
+                active = false;
+                (owner as GameWorld).StartChangeWorld(target_world, target_level);
+            }
+        }
+
+        public static Color color = Color.FromArgb(0, 200, ANY);
+
+        public static Color GetColor()
+        {
+            return color;
+        }
+
+        public override Color GetTriggerColor()
+        {
+            return GetColor();
+        }
+    }
+}
