acf145735686882b9a69733afe40ba7bc696366b
Mike Schwörer <mailport@mikescher.de>
2013-09-28 14:59:52 +0200

    Added Map 03-04

--- Program.cs
+++ Program.cs
@@ -30,7 +30,7 @@ namespace SuperBitBros
 
 
             Console.Out.WriteLine("START");
-            GameModel world = new GameWorld(3, 2);
+            GameModel world = new GameWorld(3, 3);
             OpenGLView view = new OpenGLGameView(world);
 
             Textures.Load();
--- ResourceAccessor.cs
+++ ResourceAccessor.cs
@@ -61,8 +61,7 @@ namespace SuperBitBros
                         case 2:
                             return Resources.map_03_02;
                         case 3:
-                            //return Resources.map_03_03;
-                            throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
+                            return Resources.map_03_03;
                         case 4:
                             //return Resources.map_03_04;
                             throw new NotImplementedException(String.Format("Map {0}-{1} not implemented", world, lvl));
