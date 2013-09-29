6d66d7c1c76b4f3ae66fdcbc07a1a49991abb617
Mike Schwörer <mailport@mikescher.de>
2013-09-29 14:02:44 +0200

    Added Map 04-04

--- Program.cs
+++ Program.cs
@@ -30,7 +30,7 @@ namespace SuperBitBros
 
 
             Console.Out.WriteLine("START");
-            GameModel world = new GameWorld(4, 3);
+            GameModel world = new GameWorld(4, 4);
             OpenGLView view = new OpenGLGameView(world);
 
             Textures.Load();
